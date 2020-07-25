f387abcedd06bd0246e7e5706cbb8c9e5f8ac670
mohamadrezakoohkan <mohamad_koohkan@icloud.com>
2020-07-25 11:17:10 +0430

    xcodeproject created

---
+++ Socialteq/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Socialteq
+//
+//  Created by Mohammad reza Koohkan on 5/4/1399 AP.
+//  Copyright © 1399 AP Mohamadreza Koohkan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SocialteqTests/SocialteqTests.swift
@@ -0,0 +1,34 @@
+//
+//  SocialteqTests.swift
+//  SocialteqTests
+//
+//  Created by Mohammad reza Koohkan on 5/4/1399 AP.
+//  Copyright © 1399 AP Mohamadreza Koohkan. All rights reserved.
+//
+
+import XCTest
+@testable import Socialteq
+
+class SocialteqTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
