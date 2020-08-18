fd8a80ba9e9f1423366236b86a6ab608e6387b78
mohamadrezakoohkan <mohamad_koohkan@icloud.com>
2020-08-18 20:18:42 +0430

    update linter workflow, add script

---
+++ Script/lint.sh
@@ -0,0 +1,6 @@
+swiftlint --reporter csv > Report/Swiftlint/lint-report.csv \
+  && swiftlint --reporter html > Report/Swiftlint/lint-report.html \
+  && swiftlint --reporter json > Report/Swiftlint/lint-report.json \
+  && swiftlint --reporter xcode > Report/Swiftlint/lint-report.txt \
+  && swiftlint --reporter markdown > Report/Swiftlint/lint-report.md \
+
