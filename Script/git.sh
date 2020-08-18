0b531baeab60ca86392e7060987fe7b9fb38586a
mohamadrezakoohkan <mohamad_koohkan@icloud.com>
2020-08-18 20:33:55 +0430

    updated git script

--- Script/git.sh
+++ Script/git.sh
@@ -1,8 +1,11 @@
 #!/bin/sh 
+
+. Script/foundation.sh
+
 MESSAGE="$1"
 git add . -A
-echo "All files added"
+print "All files added"
 git commit -m "$MESSAGE"
-echo "Commit added"
+print "Commit added"
 git push -u origin HEAD
-echo "Commit pushed"
+print "Commit pushed"
